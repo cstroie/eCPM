5cc38584beb3d2372cac86053dc44e21d58948bf
Costin STROIE <costinstroie@eridu.eu.org>
2020-09-09 13:40:09 +0300

    Local configuration.

---
+++ config.h
@@ -0,0 +1,33 @@
+/**
+  config.h - Common configuration
+
+  Copyright (C) 2020 Costin STROIE <costinstroie@eridu.eu.org>
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Debug mode
+//#define DEBUG
+
+// EEPROM debug mode
+//#define DEBUG_EE
+
+// SPI speed
+#define SPI_SPEED (16000000)
+
+
+#endif /* CONFIG_H */
